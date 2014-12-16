22337858365ab762bad496505917ea58b07f5e36
Fnord! <andrew.96@mail.ru>
2014-12-16 15:38:44 +0300

    FirstCommit

---
+++ TeamProjectHse272-2/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectHse272_2
+{
+    interface IProductRepository
+    {
+        IQueryable<Product> GetProducts();
+    }
+}
---
+++ TeamProjectHse272-2/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectHse272_2
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public virtual Category category { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+    }
+}
