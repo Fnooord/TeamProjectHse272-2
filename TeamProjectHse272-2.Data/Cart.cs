ee6d4eef19e2057f63f916041738d142d241cd55
Fnord! <andrew.96@mail.ru>
2014-12-17 21:57:39 +0300

    CartClassAdded

---
+++ TeamProjectHse272-2.Data/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectHse272_2.Data
+{
+    class Cart
+    {
+        public List<Product> ProductsInCart { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int TotalQuantity {get; set; }
+        public decimal GetTotalPrice (List<Product> list)
+        {
+            decimal total = 0;
+            foreach (Product p in list)
+            {
+                total += (p.Price * p.InCart);
+            }
+            return total;
+        }
+    }
+}
